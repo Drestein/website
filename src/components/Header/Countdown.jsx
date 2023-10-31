cad591340184512f9593610696e13c7ba46f5e4e
unknown <sugavarathan003@gmail.com>
2023-10-31 12:49:39 +0530

    condown

--- src/components/Header/Countdown.jsx
+++ src/components/Header/Countdown.jsx
@@ -2,62 +2,37 @@ import React, { useEffect, useState } from "react";
 import "./styles/countdown.scss";
 
 const CountDown = () => {
-  const end = new Date();
-  end.setFullYear(2023);
-  end.setMonth(10);
-  end.setDate(5);
-  end.setHours(2);
-  end.setMinutes(60);
+  // Set the end date
+  const end = new Date(2023, 10, 3, 2, 0);
 
-  const [daysDiff, setdaysDiff] = useState(0);
-  const [hoursDiff, sethoursDiff] = useState(0);
-  const [minutesDiff, setminutesDiff] = useState(0);
-  const [secondsDiff, setsecondsDiff] = useState(0);
+  const [timeDiff, setTimeDiff] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
     if (end.getTime() - new Date().getTime() > 0) {
       const timer = setInterval(() => {
         const msDiff = end.getTime() - new Date().getTime();
-        const daysDiff = Math.trunc(msDiff / (1000 * 3600 * 24));
-        setdaysDiff(daysDiff);
-        sethoursDiff(23 - new Date().getHours());
-        setminutesDiff(59 - new Date().getMinutes());
-        setsecondsDiff(59 - new Date().getSeconds());
+        const days = Math.trunc(msDiff / (1000 * 3600 * 24));
+        const hours = Math.trunc((msDiff / (1000 * 3600)) % 24);
+        const minutes = Math.trunc((msDiff / (1000 * 60)) % 60);
+        const seconds = Math.trunc((msDiff / 1000) % 60);
+        setTimeDiff({ days, hours, minutes, seconds });
       }, 1000);
       return () => {
         clearInterval(timer);
       };
     }
-  });
+  }, []);
+
   const DeviceSize = window.innerWidth;
+  const timeUnitLabel = DeviceSize < 600 ? ["Days", "Hrs", "Mins", "Secs"] : ["Days", "Hours", "Minutes", "Seconds"];
 
   return (
     <div className="countdown-container">
-      <div className="days">
-        Days <div id="countdown-card">{daysDiff}</div>
-      </div>
-      <div className="hours">
-        Hours <div id="countdown-card">{hoursDiff}</div>
-      </div>
-      {DeviceSize < 600 ? (
-        <>
-          <div className="mins">
-            Mins <div id="countdown-card">{minutesDiff}</div>
-          </div>
-          <div className="secs">
-            Secs <div id="countdown-card">{secondsDiff}</div>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="mins">
-            Minutes <div id="countdown-card">{minutesDiff}</div>
-          </div>
-          <div className="secs">
-            Seconds <div id="countdown-card">{secondsDiff}</div>
-          </div>
-        </>
-      )}
+      {Object.keys(timeDiff).map((unit, index) => (
+        <div className={unit.toLowerCase()} key={unit}>
+          {timeUnitLabel[index]} <div id="countdown-card">{timeDiff[unit]}</div>
+        </div>
+      ))}
     </div>
   );
 };
